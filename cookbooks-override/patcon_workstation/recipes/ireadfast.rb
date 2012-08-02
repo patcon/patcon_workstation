b4f2b2d95462c27b135cd798611f7a336b2b714a
Patrick Connolly <patrick@myplanetdigital.com>
2012-08-02 10:52:45 -0400

    Added iReadFast.

---
+++ cookbooks-override/patcon_workstation/recipes/ireadfast.rb
@@ -0,0 +1,5 @@
+dmg_package "iReadFast" do
+  volumes_dir "iReadFast 2.0"
+  source "http://www.gengis.net/downloads/iReadFast%202.0.dmg"
+  checksum "0bc213c6da72a7917ceba8a9de46e307933608cd6d2bf397770517401ab3d98c"
+end
